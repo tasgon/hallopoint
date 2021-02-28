b8d933bf045677bbe9eab409ac5c2b7c10f36598
tasgon <tasgon_@outlook.com>
2021-02-27 21:28:33 -0500

    update everything

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ extern crate ggez;
 
 mod imgui_wrapper;
 
+use std::slice::Windows;
+
 use crate::imgui_wrapper::ImGuiWrapper;
 use ggez::conf;
 use ggez::event::{self, EventHandler, KeyCode, KeyMods, MouseButton};
 use ggez::graphics;
-use ggez::nalgebra as na;
+use ggez::mint;
+//use ggez::nalgebra as na;
 use ggez::{Context, GameResult};
+
+use imgui::im_str;
 
 struct MainState {
     pos_x: f32,
@@ -29,29 +34,33 @@ impl MainState {
 
 impl EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+        //self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, graphics::BLACK);
+        graphics::clear(ctx, graphics::Color::BLACK);
 
         // Render game stuff
         {
             let circle = graphics::Mesh::new_circle(
                 ctx,
                 graphics::DrawMode::fill(),
-                na::Point2::new(self.pos_x, 380.0),
+                [self.pos_x, 380.0f32],
                 100.0,
                 2.0,
-                graphics::WHITE,
+                graphics::Color::WHITE,
             )?;
-            graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
+            graphics::draw(ctx, &circle, ([0.0, 0.0],))?;
         }
 
         // Render game ui
         {
-            self.imgui_wrapper.render(ctx, self.hidpi_factor);
+            self.imgui_wrapper.render(ctx, self.hidpi_factor, |ui| {
+                ui.show_demo_window(&mut true);
+                imgui::Window::new(im_str!("Main"))
+                    .build(ui, || if ui.small_button(im_str!("Open song")) {});
+            });
         }
 
         graphics::present(ctx)?;
@@ -59,6 +68,9 @@ impl EventHandler for MainState {
     }
 
     fn mouse_motion_event(&mut self, _ctx: &mut Context, x: f32, y: f32, _dx: f32, _dy: f32) {
+        //let x = x / 2f32;
+        //let y = y / 2f32;
+        self.pos_x = x;
         self.imgui_wrapper.update_mouse_pos(x, y);
     }
 
@@ -72,13 +84,7 @@ impl EventHandler for MainState {
         self.imgui_wrapper.update_mouse_down(button);
     }
 
-    fn mouse_button_up_event(
-        &mut self,
-        _ctx: &mut Context,
-        button: MouseButton,
-        _x: f32,
-        _y: f32,
-    ) {
+    fn mouse_button_up_event(&mut self, _ctx: &mut Context, button: MouseButton, _x: f32, _y: f32) {
         self.imgui_wrapper.update_mouse_up(button);
     }
 
@@ -103,6 +109,7 @@ impl EventHandler for MainState {
     fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
         graphics::set_screen_coordinates(ctx, graphics::Rect::new(0.0, 0.0, width, height))
             .unwrap();
+        println!("{}", width);
         //println!("{:?}", graphics::screen_coordinates(ctx));
     }
 
@@ -117,12 +124,13 @@ pub fn main() -> ggez::GameResult {
         .window_mode(
             conf::WindowMode::default().resizable(true), /*.dimensions(750.0, 500.0)*/
         );
-    let (ref mut ctx, event_loop) = &mut cb.build()?;
+    let (mut ctx, event_loop) = cb.build()?;
 
-    let hidpi_factor = event_loop.get_primary_monitor().get_hidpi_factor() as f32;
+    let hidpi_factor = event_loop.primary_monitor().unwrap().scale_factor() as f32;
+    //let hidpi_factor = 1.0f32;
     println!("main hidpi_factor = {}", hidpi_factor);
 
-    let state = &mut MainState::new(ctx, hidpi_factor)?;
+    let state = MainState::new(&mut ctx, hidpi_factor)?;
 
     event::run(ctx, event_loop, state)
 }
