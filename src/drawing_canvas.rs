db6ca65d6f59abf5686865d7a3a7a779d9fa04f1
telastrus <10052313+telastrus@users.noreply.github.com>
2021-03-01 03:01:39 -0500

    it wasnt hidpi that hurt me, it was screen coordinates

--- src/drawing_canvas.rs
+++ src/drawing_canvas.rs
@@ -1,7 +1,10 @@
-use ggez::graphics;
 use ggez::graphics::{Canvas, DrawMode, DrawParam, FillOptions};
 use ggez::mint::Point2;
-use graphics::{get_window_color_format, Color, MeshBuilder};
+use ggez::{graphics, Context};
+use graphics::{
+    get_window_color_format, screen_coordinates, set_screen_coordinates, size, Color, MeshBuilder,
+    Rect,
+};
 
 pub struct DrawingCanvas {
     canvas: Canvas,
@@ -26,6 +29,7 @@ impl DrawingCanvas {
             brush_radius: 3.0f32,
         }
     }
+
     pub fn stroke(
         &mut self,
         last_point: impl Into<Option<Point2<f32>>>,
@@ -34,30 +38,24 @@ impl DrawingCanvas {
         ctx: &mut ggez::Context,
     ) {
         let point = point.into();
-        //point.x *= 1.25f32;
+        let old_rect = graphics::screen_coordinates(ctx);
         let color = graphics::Color::new(0.0f32, 0.0f32, 0.0f32, pressure);
         graphics::set_canvas(ctx, Some(&self.canvas));
+
+        // This is abhorrent but `https://github.com/ggez/ggez/issues/497` necessitates it.
+        graphics::set_screen_coordinates(ctx, Rect::new(0f32, 0f32, 500f32, 500f32)).unwrap();
+
         let mut mb = MeshBuilder::new();
         if let Some(lp) = last_point.into() {
-            //lp.x *= 1.25f32;
-            /*let dx = point.x - lp.x;
-            let dy = point.y - lp.y;*/
             if let Err(e) = mb.line(&[lp, point], self.brush_radius * 2.0f32, color) {
                 println!("{:?}", e);
             }
         }
-        /*mb.circle(
-            DrawMode::Fill(FillOptions::DEFAULT),
-            point,
-            self.brush_radius,
-            1.0f32,
-            color,
-        )
-        .unwrap();*/
         if let Ok(mesh) = mb.build(ctx) {
             graphics::draw(ctx, &mesh, ([0.0, 0.0],)).unwrap();
         }
         graphics::set_canvas(ctx, None);
+        graphics::set_screen_coordinates(ctx, old_rect).unwrap();
     }
 
     pub fn clear(&mut self, ctx: &mut ggez::Context) {
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ extern crate ggez;
 mod drawing_canvas;
 mod imgui_wrapper;
 
+use std::sync::mpsc::channel;
+
 use crate::imgui_wrapper::ImGuiWrapper;
 use drawing_canvas::DrawingCanvas;
-use ggez::event::winit_event::{Force, Touch, WindowEvent};
 use ggez::event::{self, EventHandler, KeyCode, KeyMods, MouseButton};
 use ggez::graphics;
 use ggez::{conf, mint::Point2};
+use ggez::{
+    event::winit_event::{Force, Touch, WindowEvent},
+    graphics::DrawMode,
+};
 use ggez::{Context, GameResult};
 
+use graphics::{Color, FillOptions, StrokeOptions};
 use imgui::im_str;
 
 struct MainState {
@@ -44,27 +50,58 @@ impl EventHandler for MainState {
 
         // Render game stuff
         {
-            //graphics::draw(ctx, &, ([0.0, 0.0],))?;
             self.board.draw(ctx, None);
+            if let Some(t) = self.last_touch {
+                let p = Point2 {
+                    x: t.location.x as f32,
+                    y: t.location.y as f32,
+                };
+                let cursor = graphics::Mesh::new_circle(
+                    ctx,
+                    DrawMode::Stroke(StrokeOptions::DEFAULT),
+                    p,
+                    3f32,
+                    1f32,
+                    Color::new(1f32, 0f32, 0f32, 1f32),
+                )
+                .unwrap();
+                graphics::draw(ctx, &cursor, ([0.0, 0.0],))?;
+            }
         }
 
         let touch = self.last_touch;
         let clear_board = &mut false;
+        let mut val = self.board.brush_radius as i32;
+
+        let (sender, receiver) = channel::<Box<dyn FnMut(&mut Context)>>();
 
         // Render game ui
         {
             self.imgui_wrapper.render(ctx, |ui| {
                 ui.show_demo_window(&mut true);
                 imgui::Window::new(im_str!("Main")).build(ui, || {
-                    if ui.small_button(im_str!("Clear")) {
-                        *clear_board = true;
+                    if ui.small_button(im_str!("Hide cursor")) {
+                        sender
+                            .send(Box::new(|ctx2| {
+                                ggez::input::mouse::set_cursor_hidden(ctx2, true);
+                            }))
+                            .unwrap();
                     }
                     if let Some(t) = touch {
                         ui.text(im_str!("{:?}\n{:?}", t.location, t.force));
                     }
+                    imgui::Slider::new(im_str!("Stroke size"))
+                        .range(1..=10)
+                        .build(ui, &mut val);
                 });
             });
         }
+
+        while let Ok(mut f) = receiver.try_recv() {
+            f(ctx);
+        }
+
+        self.board.brush_radius = val as f32;
 
         if *clear_board {
             println!("{}", *clear_board);
@@ -112,6 +149,7 @@ impl EventHandler for MainState {
     }
 
     fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        self.board = DrawingCanvas::new(ctx);
         graphics::set_screen_coordinates(ctx, graphics::Rect::new(0.0, 0.0, width, height))
             .unwrap();
     }
@@ -161,6 +199,7 @@ pub fn main() -> ggez::GameResult {
             conf::WindowMode::default().resizable(true), /*.dimensions(750.0, 500.0)*/
         );
     let (mut ctx, event_loop) = cb.build()?;
+    //ggez::input::mouse::set_cursor_hidden(&mut ctx, true);
 
     let hidpi_factor = event_loop.primary_monitor().unwrap().scale_factor() as f32;
     println!("dpi: {}", hidpi_factor);
